35ffb1a3f0687b3628556c7cc65494b22854ed17
mikaelmendoza21 <mikaelmendoza21@gmail.com>
2019-03-31 22:12:22 -0700

    Styling updates and added new modal feature to show coding experience.
    Signed-off-by: mikaelmendoza21

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var greeting = 'dear visitor';
+var $modal;
 
 $(document).ready(function(){
 	console.log(`Hello ${greeting}!`);
 	console.log('Mikael A. Mendoza wuz here ;)');
+
+	$modal = $('#modal-popup');
 });
+
+function showCodingExperience(){
+	console.log(`Let's see my experience as of ${new Date().toDateString()}`);
+
+	var codingHistoryTitle = 'I have experience with:';
+	var codingExperience = ['C# .Net', 'PHP', 'Node.js', 'Java Android'];
+
+	$modal.find('.modal-title').html(codingHistoryTitle);
+	$modal.find('.modal-content-text').html(codingExperience.join('<br/>'));
+
+	$modal.modal('show');
+}
